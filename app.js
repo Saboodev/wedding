a5073afac23dba3e8082b7994078dfd6b9d8fd0f
saboodev <sabrina_desantis@hotmail.fr>
2024-03-17 02:08:24 +0100

    Premier commit

---
+++ app.js
@@ -0,0 +1,23 @@
+(function () {
+    [...document.querySelectorAll(".nav")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    [...document.querySelectorAll(".cta")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    })
+})();
+
+const activeBtn = document.querySelector(".active-btn");
+if (activeBtn) {
+    activeBtn.classList.remove("active-btn");
+}
