d99abedd8cb1c42520a1034fda3922238177fd43
saboodev <sabrina_desantis@hotmail.fr>
2024-03-27 00:06:52 +0100

    ajout page hebergement et liste hotel

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-(function () {
-    [...document.querySelectorAll(".nav")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
-    });
-    [...document.querySelectorAll(".cta")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
-    })
-})();
+<script>
+    (function () {
+        document.querySelectorAll(".nav").forEach(button => {
+            button.addEventListener("click", function () {
+                document.querySelector(".active-btn").classList.remove("active-btn");
+                this.classList.add("active-btn");
+                document.querySelector(".active").classList.remove("active");
+                document.getElementById(button.dataset.id).classList.add("active");
+            });
+        });
 
-const activeBtn = document.querySelector(".active-btn");
-if (activeBtn) {
-    activeBtn.classList.remove("active-btn");
-}
+        document.querySelectorAll(".cta").forEach(button => {
+            button.addEventListener("click", function () {
+                document.querySelector(".active-btn").classList.remove("active-btn");
+                this.classList.add("active-btn");
+                document.querySelector(".active").classList.remove("active");
+                document.getElementById(button.dataset.id).classList.add("active");
+            });
+        });
+    })();
+</script>
