5ba490d3909bfbb0c6b327a77a1b6244cc5616f4
saboodev <sabrina_desantis@hotmail.fr>
2024-04-03 23:51:30 +0200

    site sans mdp

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
-// Encapsulez votre code dans une fonction auto-exécutante
-(function() {
-    document.querySelectorAll(".li-content").forEach(button => {
-        button.addEventListener("click", function () {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        });
-    });
-})();
-
 // Ajoutez cette fonction dans votre app.js
 function checkPassword() {
     var passwordInput = document.getElementById("passwordInput").value;
     var correctPassword = "0510";
 
     if (passwordInput === correctPassword) {
+        console.log("Mot de passe correct. Redirection en cours...");
         document.getElementById("passwordMessage").textContent = "Mot de passe correct. Redirection en cours...";
         // Redirigez l'utilisateur vers la page principale ou déverrouillez le contenu ici
         setTimeout(function() {
+            console.log("Redirection vers index.html");
             window.location.href = "index.html"; // Redirigez vers la page principale
-        }, 2000); // Redirige après 2 secondes (2000 millisecondes)
+        }, 6000); // Redirige après 2 secondes (2000 millisecondes)
     } else {
+        console.log("Mot de passe incorrect");
         document.getElementById("passwordMessage").textContent = "Mot de passe incorrect. Veuillez réessayer.";
     }
 }
-
 
 // Ajoutez cette fonction pour gérer le menu burger
 function toggleMenu() {
